3968e65c5ea1c482924c6dc4b085c235004d6629
Walmyr <wlsf82@gmail.com>
2024-06-16 18:20:49 +0200

    Write the first test and update npm scripts

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+    cy.visit('./src/index.html')
+
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 })
