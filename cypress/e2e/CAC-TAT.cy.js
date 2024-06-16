f9b7eb0695ac09d6fbd474f4d217d61a8aeb8348
Walmyr <wlsf82@gmail.com>
2024-06-16 19:43:33 +0200

    Solve the first extra exercise of lesson 7

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -156,4 +156,12 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('have.attr', 'href', 'privacy.html')
       .and('have.attr', 'target', '_blank')
   })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+  })
 })
