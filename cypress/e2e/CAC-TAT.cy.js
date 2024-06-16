e0da51514d90bb08d45120df2164e05d09a6ec97
Walmyr <wlsf82@gmail.com>
2024-06-16 19:33:55 +0200

    Solve the first exercise of lesson 3

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -82,4 +82,10 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('.success').should('be.visible')
   })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  })
 })
