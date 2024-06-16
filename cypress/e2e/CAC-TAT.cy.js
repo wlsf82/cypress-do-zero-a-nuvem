eb2393291bf428beb98041bc15c968f3643a5149
Walmyr <wlsf82@gmail.com>
2024-06-16 20:21:22 +0200

    Solve the last challenge 🎉

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -229,4 +229,14 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .its('body')
       .should('include', 'CAC TAT')
   })
+
+  it('encontra o gato escondido', () => {
+    cy.get('#cat')
+      .invoke('show')
+      .should('be.visible')
+    cy.get('#title')
+      .invoke('text', 'CAT TAT')
+    cy.get('#subtitle')
+      .invoke('text', 'Eu 🩷 gatos!')
+  })
 })
