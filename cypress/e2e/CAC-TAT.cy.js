8aca134ea2d146cbf7482844be236d42a6cc127a
Veron4M4zz4 <mazza.verona@gmail.com>
2025-01-28 11:50:01 -0300

    :white_check_mark: Created the news test suits on Cypress for the core infos to submit

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,21 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o título da aplicação', () => {
+  beforeEach(() => { 
     cy.visit('./src/index.html')
+  })
+
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('John')
+    cy.get('#lastName').type('Doe')
+    cy.get('#email').type('johndoe@gmail.com')
+    cy.get('#support-type > :nth-child(3)')
+    cy.get('#open-text-area').type('Obrigado por tudo!')
+    cy.get('.button').click()
+
+
+    cy.get('.success').should('be.visible')
   })
 })
