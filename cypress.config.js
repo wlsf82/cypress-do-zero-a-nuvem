8c2b70b1656dccd6eb646914137aa35bb08243cb
Veron4M4zz4 <mazza.verona@gmail.com>
2025-01-28 12:52:54 -0300

    :white_check_mark: Added the repeat function on e2e formulary test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportHeight: 880,
-  viewportWidth: 1280,
-  e2e: {},
-})
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -20,4 +20,34 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('.success').should('be.visible')
   })
+  
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('John')
+    cy.get('#lastName').type('Doe')
+    cy.get('#email').type('johndoe@gmail,com')
+    cy.get('#support-type > :nth-child(3)')
+    cy.get('#open-text-area').type('Obrigado')
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('Campo telefone continua vazio quando preenchido com um valor não-numérico', () =>{ 
+    cy.get('#phone')
+      .type('abcde')
+      .should('have.value', '')
+
+  })
+
+  it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('John')
+    cy.get('#lastName').type('Doe')
+    cy.get('#email').type('johndoe@gmail,com')
+    cy.get('#support-type > :nth-child(3)')
+    cy.get('#open-text-area').type('Obrigado')
+    cy.get('[for="phone-checkbox"]').click()
+    cy.get('.button[type="submit"]').click()
+    
+    cy.get('.error').should('be.visible')
+  })
 })
