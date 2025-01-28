a73c4347f99f785035de5b3ca50bfe0de9c9f41f
Veron4M4zz4 <mazza.verona@gmail.com>
2025-01-28 14:23:43 -0300

    :white_check_mark: Tested the select inputs with differents ways to click

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -39,7 +39,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   })
 
-  it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
     cy.get('#firstName').type('John')
     cy.get('#lastName').type('Doe')
     cy.get('#email').type('johndoe@gmail,com')
@@ -50,4 +50,55 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     
     cy.get('.error').should('be.visible')
   })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('John')
+        .should('have.value', 'John') 
+        .clear()
+        .should('have.value', '')
+     cy.get('#lastName')
+      .type('Doe Silva')
+        .should('have.value', 'Doe Silva') 
+        .clear()
+        .should('have.value', '')
+      cy.get('#email')
+        .type('johndoe@gmail.com')
+          .should('have.value', 'johndoe@gmail.com') 
+          .clear()
+          .should('have.value', '')
+      cy.get('#phone')
+          .type('123456789')
+            .should('have.value', '123456789') 
+            .clear()
+            .should('have.value', '')
+  })
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('.button[type="submit"]').click()
+  })
+  
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+  })
+it('seleciona um produto (YouTube) por seu texto', () => {
+  cy.get('#product')
+    .select('youtube')
+    .should('have.value', 'youtube')
 })
+
+it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+  cy.get('#product')
+    .select('mentoria')
+    .should('have.value', 'mentoria')
+})
+
+it('seleciona um produto (Blog) por seu índice', () => {
+  cy.get('#product')
+    .select(1)
+    .should('have.value', 'blog')
+})
+
+})
+
