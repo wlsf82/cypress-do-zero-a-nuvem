b2f857bb70fccdb03c1207652a990c93e8782547
Tibério Neto <athanetto@gmail.com>
2025-01-29 21:25:31 -0300

    Meu primeiro teste

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('Verifica o título da aplicação', () => {
+    cy.visit("./src/index.html")
+
+    cy.title().should("be.equal","Central de Atendimento ao Cliente TAT")
   })
 })
