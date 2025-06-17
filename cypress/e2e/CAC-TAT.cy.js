7b238d18b86ed9a718b1973ddf5ab53f4d0c3930
PedroHLeao <phfleao@gmail.com>
2025-06-16 21:48:12 -0300

    Inicio do curso

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,6 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+    cy.visit('./src/index.html')
+    cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+  })
+})
